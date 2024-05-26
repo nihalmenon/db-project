cba4492c5c002eb665adf8065e759c30418bcb38
Kieran Hulsman <kieran.hulsman@uwaterloo.ca>
2024-05-25 21:51:52 -0400

    integration setup

---
+++ client/src/actions/baseActions.ts
@@ -0,0 +1,16 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { baseApiUrl } from "../config/config";
+
+const fullEndpoint = (endpoint: string): string => {
+  if (endpoint.startsWith("http")) {
+    return `/${endpoint}`;
+  } else {
+    return `/${baseApiUrl}/${endpoint}`;
+  }
+}
+
+const get = (endpoint: string, config?: AxiosRequestConfig) => {
+  return axios.get(fullEndpoint(endpoint), config);
+}
+
+export { get };
