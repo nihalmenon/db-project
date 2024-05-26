e15613ce01734899b4ba318315e67c9bd14467eb
Nihal Menon <=>
2024-05-25 21:46:00 -0400

    setup connection + hello-world endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,24 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
 
-const app = express()
+const { dbConnection } = require('./connection');
+const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-    res.status(200).send("Hello World!")
-})
+    res.status(200).send("Hello World!");
+});
 
-app.use(express.json())
+app.get('/hello-world', (req: Request, res: Response) => {
+    dbConnection.query("SELECT * FROM User", function(err: Error, result: any) {
+        if (err) {
+            console.error(err);
+            res.status(500).send("An error occurred.");
+            return;
+        }
+        console.log("Result: " + result);
+        res.status(200).send(result);
+    });
+});
 
-module.exports = app
+app.use(express.json());
+
+module.exports = app;
