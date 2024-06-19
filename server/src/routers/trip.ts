aefcf69ae5b91650e8454e1779160a38295106c7
Nihal Menon <=>
2024-06-19 15:33:10 -0400

    auth v2

---
+++ server/src/routers/trip.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import connection from '../connection';
+import auth from '../middleware/authMiddleware';
+
+
+const router = express.Router();
+
+// get all trips protected route
+router.get('/', auth, (req, res) => {
+    const query = 'SELECT * FROM Trip';
+    connection.query(query, (err: Error, results: any[]) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('An error occurred while fetching trips');
+        }
+        res.status(200).json(results);
+    });
+});
+
+export default router;
