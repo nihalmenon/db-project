653c2b7ba68997691343729d8478abaa14a1689f
Kieran Hulsman <kieran.hulsman@uwaterloo.ca>
2024-06-19 16:14:25 -0400

    add trip member sp

--- server/db/storedProcedures/create_trip.sql
+++ server/db/storedProcedures/create_trip.sql
@@ -1,7 +1,7 @@
 delimiter //
-create procedure create_trip(in _lid int, in _bio nvarchar(2048))
+create procedure create_trip(in _uid int, in _lid int, in _bio nvarchar(2048))
 begin
-	insert into Trip (lid, bio) 
-    values (_lid, _bio);
+	insert into Trip (lid, bio) values (_lid, _bio);
+    insert into Member (uid, lid) values (_uid, _lid);
 end //
 delimiter ;
