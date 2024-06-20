1ef1594c847244d66bbcd80ab091fad60df45592
Nihal Menon <=>
2024-06-19 20:03:25 -0400

    typo

--- server/db/storedProcedures/search_match_trips.sql
+++ server/db/storedProcedures/search_match_trips.sql
@@ -1,7 +1,7 @@
 delimiter $$
 create procedure search_match_trips(in _tid int)
 begin
-    SELECT FROM Trip t1, Trip t2
+    SELECT * FROM Trip t1, Trip t2
     INNER JOIN Member m ON t.tid = m.tid
     INNER JOIN User u ON m.uid = u.uid
     WHERE t1.tid = _tid AND t1.tid != t2.tid AND t1.lid = t2.lid AND t1.start_date <= t2.end_date AND t1.end_date >= t2.start_date;
